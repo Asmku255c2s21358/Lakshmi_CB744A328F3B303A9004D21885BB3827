1f9f4db35510e0304f73f37588ce823823f93c62
Asmku255c2s21358 <148339564+Asmku255c2s21358@users.noreply.github.com>
2023-10-18 20:14:20 +0530

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,16 @@
+,,,,,, 
+
+year%100! =0/
+year%400==0
+,,,,,, 
+
+definitely is leap year (year):
+if(year%4==0 and year%100! =0)
+or year%400==0:
+return TrueTrue
+eise:
+return False
+
+year=ihhzpprint (nt(input(''enter a year''))
+if is leap year (year) :
+p))))(''leap year'')
